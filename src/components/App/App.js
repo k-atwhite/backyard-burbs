6e6cb78d30031b699a4009de929da89069dc27cc
Lauren Kessell <lkessell1@gmail.com>
2021-07-25 17:07:42 -0400

    Add NavBar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import NavBar from '../NavBar/NavBar';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Backyard Burbs</p>
+        <NavBar />
       </header>
     </div>
   );
