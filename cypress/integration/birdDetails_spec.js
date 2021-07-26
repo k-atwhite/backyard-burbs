6c56a0d7be1daf06e35e4187d43b530d1abe924b
Kat White <kat.white.a@gmail.com>
2021-07-26 19:11:26 -0400

    Add test requirements for birdDetail page

---
+++ cypress/integration/birdDetails_spec.js
@@ -0,0 +1,11 @@
+beforeEach(() => {});
+
+describe("Bird Details", () => {
+  it("Should visit the url with the specific bird key", () => {});
+
+  it("Should contain details about the bird", () => {});
+
+  it("Should have an I SAW IT! button", () => {});
+
+  it("Should display the nav bar with home button, and My Birds button", () => {});
+});
