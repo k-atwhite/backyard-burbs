ce02d4cd89833e4fd255e7fa0129c38874a55eed
Sydney Bear <sbear3915@gmail.com>
2021-07-26 15:49:48 -0600

    Render additional bird details for the bird that was selected

--- src/components/Bird/Bird.js
+++ src/components/Bird/Bird.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Bird.css";
+import { Link } from 'react-router-dom';
 
 const Bird = ({ commonName, scientificName, id }) => {
   return (
-    <div className="bird-card">
-      <h3>{commonName}</h3>
-      <p>{scientificName}</p>
-      <button>🔍</button>
-    </div>
+    <Link to={`/birds/${id}`}>
+      <div className="bird-card">
+        <h3>{commonName}</h3>
+        <p>{scientificName}</p>
+        <button>🔍</button>
+      </div>
+    </Link>
+    
   );
 };
 
