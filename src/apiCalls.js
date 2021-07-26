cb7b40209de0afddec626f4182d25676d2e6ece3
Lauren Kessell <lkessell1@gmail.com>
2021-07-26 16:10:42 -0400

    Switch to response.json

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,5 +3,5 @@ const requestOptions = { method: 'GET', headers: myHeaders, redirect: 'follow' }
 
 export const getBirds = (currentState) => {
   return fetch(`https://api.ebird.org/v2/data/obs/US-${currentState}/recent`, requestOptions)
-  .then(response => response.text())
+  .then(response => response.json())
 }
