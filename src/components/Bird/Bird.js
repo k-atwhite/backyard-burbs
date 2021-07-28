95091d44405afca88f2371d5da4974b1de71bd7a
Lauren Kessell <lkessell1@gmail.com>
2021-07-28 18:23:05 -0400

    Fetch images when each Bird card is rendered

--- src/components/Bird/Bird.js
+++ src/components/Bird/Bird.js
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import "./Bird.css";
 import { Link } from "react-router-dom";
+import { getImages } from '../../apiCalls';
 import ThemeContext from "../ThemeContext";
 
 const Bird = ({ commonName, scientificName, id }) => {
   const value = useContext(ThemeContext);
+  getImages(commonName)
+    .then(data => console.log(data))
   return (
     <Link to={`/birds/${id}`}>
       <div className={`bird-card ${value}`}>
