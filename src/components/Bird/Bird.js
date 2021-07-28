95c995d287543de30c301af3fe8fba247e231410
Lauren Kessell <lkessell1@gmail.com>
2021-07-28 19:23:33 -0400

    Fetch and render an image for each card

--- src/components/Bird/Bird.js
+++ src/components/Bird/Bird.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Bird.css";
 import { Link } from "react-router-dom";
 import { getImages } from '../../apiCalls';
@@ -6,11 +6,27 @@ import ThemeContext from "../ThemeContext";
 
 const Bird = ({ commonName, scientificName, id }) => {
   const value = useContext(ThemeContext);
-  getImages(commonName)
-    .then(data => console.log(data))
+  const [photos, setPhotos] = useState([]);
+
+  const createURL = (photo) => {
+    const photoUrl = 'https://live.staticflickr.com/' + photo.server + '/' +
+      photo.id + '_' + photo.secret + '_b.jpg';
+
+      return photoUrl;
+  }
+
+  getImages(scientificName)
+  .then(data => {
+    if (!photos.length) {
+      const url = createURL(data.photos.photo[0])
+      setPhotos(url)
+    }
+  });
+
   return (
     <Link to={`/birds/${id}`}>
       <div className={`bird-card ${value}`}>
+        <img src={photos} width='300px'/>
         <h3>{commonName}</h3>
         <p>{scientificName}</p>
       </div>
