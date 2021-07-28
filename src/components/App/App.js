6ebe85126e14a2141c307360505e4d68d4ee3636
Kat White <kat.white.a@gmail.com>
2021-07-28 11:53:08 -0600

    Add themeContext folder and useState for goldfinch/bluejay

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
@@ -7,19 +7,12 @@ import BirdList from "../BirdList/BirdList";
 import { getBirds } from "../../apiCalls";
 import BirdDetails from "../BirdDetails/BirdDetails";
 import MyBirds from "../MyBirds/MyBirds";
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       regionBirds: [],
-//       myBirds: [],
-//     };
-//   }
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const [regionBirds, setRegionBirds] = useState([])
-  const [myBirds, setMyBirds] = useState([])
+  const [regionBirds, setRegionBirds] = useState([]);
+  const [myBirds, setMyBirds] = useState([]);
+  const [theme, setTheme] = useState("goldfinch");
 
   const setBirds = (stateAbv) => {
     getBirds(stateAbv).then((data) => setRegionBirds(data));
@@ -33,47 +26,47 @@ const App = () => {
     setMyBirds([seenBird, ...myBirds]);
   };
 
-  const loadingMsg = !regionBirds.length && (
-      <h2>Loading your birds...</h2>
-    );
+  const toggleTheme = () => {
+    const newTheme = theme === "goldfinch" ? "blueJay" : "goldfinch";
+    setTheme(newTheme);
+  };
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>Backyard Burbs</p>
-          <NavBar />
-        </header>
-        <Switch>
-          <Route
-            path="/birds/:id"
-            render={({ match }) => {
-              const bird = regionBirds.find(
-                (bird) => bird.speciesCode === match.params.id
-              );
+  const loadingMsg = !regionBirds.length && <h2>Loading your birds...</h2>;
 
-              if (!bird) {
-                return <h2>That bird doesn't exist!</h2>;
-              }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Backyard Burbs</p>
+        <NavBar />
+      </header>
+      <Switch>
+        <Route
+          path="/birds/:id"
+          render={({ match }) => {
+            const bird = regionBirds.find(
+              (bird) => bird.speciesCode === match.params.id
+            );
 
-              return <BirdDetails bird={bird} addBird={addBird} />;
-            }}
-          ></Route>
-          <Route path="/birds">
-            {loadingMsg}
-            <BirdList birdData={regionBirds} />
-          </Route>
-          <Route path="/myBirds">
-            <MyBirds birdData={myBirds}/>
-          </Route>
-          <Route path="/">
-            <StatePicker
-              setBirds={setBirds}
-              clearBirds={clearBirds}
-            />
-          </Route>
-        </Switch>
-      </div>
-    );
-}
+            if (!bird) {
+              return <h2>That bird doesn't exist!</h2>;
+            }
+
+            return <BirdDetails bird={bird} addBird={addBird} />;
+          }}
+        ></Route>
+        <Route path="/birds">
+          {loadingMsg}
+          <BirdList birdData={regionBirds} />
+        </Route>
+        <Route path="/myBirds">
+          <MyBirds birdData={myBirds} />
+        </Route>
+        <Route path="/">
+          <StatePicker setBirds={setBirds} clearBirds={clearBirds} />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
