c28f209e85cc2315494a6f8c21ddda2e9fd9ff9f
Sydney Bear <sbear3915@gmail.com>
2021-07-29 09:49:42 -0600

    Add photos to bird detail page

--- src/components/BirdDetails/BirdDetails.js
+++ src/components/BirdDetails/BirdDetails.js
@@ -3,12 +3,43 @@ import binoculars from "../../binoculars.svg";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import ThemeContext from "../ThemeContext";
+import { getImages } from "../../apiCalls";
 
 const BirdDetails = ({ bird, addBird }) => {
   const [newBird, setNewBird] = useState(bird);
+  const [photos, setPhotos] = useState([]);
+
+  const createURL = (photo) => {
+    const photoUrl = 'https://live.staticflickr.com/' + photo.server + '/' +
+      photo.id + '_' + photo.secret + '_b.jpg';
+
+      return photoUrl;
+  }
+
+  // getImages(scientificName, 3)
+  // .then(data => {
+  //   if (!photos.length) {
+  //     const url = createURL(data.photos.photo)
+  //     setPhotos(url)
+  //   }
+  // });
+
+  getImages(bird.sciName, 3)
+    .then(data => {
+      if(!photos.length) {
+        const url = data.photos.photo.map(photo => {
+          return createURL(photo)
+        })
+        setPhotos(url)
+      }
+
+    })
 
   return (
     <div className="bird-detail">
+      <img src={photos[0]} width='300px'/>
+      <img src={photos[1]} width='300px'/>
+      <img src={photos[2]} width='300px'/>
       <h3>{bird.comName}</h3>
       <p>{bird.sciName}</p>
       <p>Last seen on: {bird.obsDt}</p>
