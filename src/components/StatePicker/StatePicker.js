b115833749d35cb35d2607c897498f27f5bd4531
Sydney Bear <sbear3915@gmail.com>
2021-07-29 15:16:26 -0600

    Add map npm

--- src/components/StatePicker/StatePicker.js
+++ src/components/StatePicker/StatePicker.js
@@ -1,82 +1,96 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './StatePicker.css';
+import USAMap from 'react-usa-map';
 
 const StatePicker = (props) => {
-  const [value, setValue] = useState('AL')
+  const [value, setValue] = useState('')
 
   const handleChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  const handleSubmit = (event) => {
+    // setValue(event.target.value)
+    // clearValue()
+    setValue(event.target.dataset.name)
+    
     event.preventDefault();
     props.clearBirds();
-    props.setBirds(value);
+    // props.setBirds(value);
+    console.log("name", event.target.dataset.name)
+    console.log(value, 'value')
   }
 
-  
+  const clearValue = () => {
+    setValue('')
+  }
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   props.clearBirds();
+  //   props.setBirds(value);
+  // }
+
     return (
-      <form className= 'dropdown'>
-        <label>
-          Select your state:
-          <select value={value} onChange={handleChange}>
-            <option value='AL'>Alabama</option>
-            <option value='AK'>Alaska</option>
-            <option value='AZ'>Arizona</option>
-            <option value='AR'>Arkansas</option>
-            <option value='CA'>California</option>
-            <option value='CO'>Colorado</option>
-            <option value='CT'>Connecticut</option>
-            <option value='DE'>Delaware</option>
-            <option value='FL'>Florida</option>
-            <option value='GA'>Georgia</option>
-            <option value='HI'>Hawaii</option>
-            <option value='ID'>Idaho</option>
-            <option value='IL'>Illinois</option>
-            <option value='IN'>Indiana</option>
-            <option value='IA'>Iowa</option>
-            <option value='KS'>Kansas</option>
-            <option value='KY'>Kentucky</option>
-            <option value='LA'>Louisiana</option>
-            <option value='ME'>Maine</option>
-            <option value='MD'>Maryland</option>
-            <option value='MA'>Massachusetts</option>
-            <option value='MI'>Michigan</option>
-            <option value='MN'>Minnesota</option>
-            <option value='MS'>Mississippi</option>
-            <option value='MO'>Missouri</option>
-            <option value='MT'>Montana</option>
-            <option value='NE'>Nebraska</option>
-            <option value='NV'>Nevada</option>
-            <option value='NH'>New Hampshire</option>
-            <option value='NJ'>New Jersey</option>
-            <option value='NM'>New Mexico</option>
-            <option value='NY'>New York</option>
-            <option value='NC'>North Carolina</option>
-            <option value='ND'>North Dakota</option>
-            <option value='OH'>Ohio</option>
-            <option value='OK'>Oklahoma</option>
-            <option value='OR'>Oregon</option>
-            <option value='PA'>Pennsylvania</option>
-            <option value='RI'>Rhode Island</option>
-            <option value='SC'>South Carolina</option>
-            <option value='SD'>South Dakota</option>
-            <option value='TN'>Tennessee</option>
-            <option value='TX'>Texas</option>
-            <option value='UT'>Utah</option>
-            <option value='VT'>Vermont</option>
-            <option value='VA'>Virginia</option>
-            <option value='WA'>Washington</option>
-            <option value='WV'>West Virginia</option>
-            <option value='WI'>Wisconsin</option>
-            <option value='WY'>Wyoming</option>
-          </select>
-        </label>
-        <button onClick={handleSubmit}>
-          <Link to='/birds'>Submit</Link>
-        </button>
-      </form>
+      <Link to='/birds'>
+        <USAMap onClick={handleChange}/>
+      </Link>
+      // <form className= 'dropdown'>
+      //   <label>
+      //     Select your state:
+      //     <select value={value} onChange={handleChange}>
+      //       <option value='AL'>Alabama</option>
+      //       <option value='AK'>Alaska</option>
+      //       <option value='AZ'>Arizona</option>
+      //       <option value='AR'>Arkansas</option>
+      //       <option value='CA'>California</option>
+      //       <option value='CO'>Colorado</option>
+      //       <option value='CT'>Connecticut</option>
+      //       <option value='DE'>Delaware</option>
+      //       <option value='FL'>Florida</option>
+      //       <option value='GA'>Georgia</option>
+      //       <option value='HI'>Hawaii</option>
+      //       <option value='ID'>Idaho</option>
+      //       <option value='IL'>Illinois</option>
+      //       <option value='IN'>Indiana</option>
+      //       <option value='IA'>Iowa</option>
+      //       <option value='KS'>Kansas</option>
+      //       <option value='KY'>Kentucky</option>
+      //       <option value='LA'>Louisiana</option>
+      //       <option value='ME'>Maine</option>
+      //       <option value='MD'>Maryland</option>
+      //       <option value='MA'>Massachusetts</option>
+      //       <option value='MI'>Michigan</option>
+      //       <option value='MN'>Minnesota</option>
+      //       <option value='MS'>Mississippi</option>
+      //       <option value='MO'>Missouri</option>
+      //       <option value='MT'>Montana</option>
+      //       <option value='NE'>Nebraska</option>
+      //       <option value='NV'>Nevada</option>
+      //       <option value='NH'>New Hampshire</option>
+      //       <option value='NJ'>New Jersey</option>
+      //       <option value='NM'>New Mexico</option>
+      //       <option value='NY'>New York</option>
+      //       <option value='NC'>North Carolina</option>
+      //       <option value='ND'>North Dakota</option>
+      //       <option value='OH'>Ohio</option>
+      //       <option value='OK'>Oklahoma</option>
+      //       <option value='OR'>Oregon</option>
+      //       <option value='PA'>Pennsylvania</option>
+      //       <option value='RI'>Rhode Island</option>
+      //       <option value='SC'>South Carolina</option>
+      //       <option value='SD'>South Dakota</option>
+      //       <option value='TN'>Tennessee</option>
+      //       <option value='TX'>Texas</option>
+      //       <option value='UT'>Utah</option>
+      //       <option value='VT'>Vermont</option>
+      //       <option value='VA'>Virginia</option>
+      //       <option value='WA'>Washington</option>
+      //       <option value='WV'>West Virginia</option>
+      //       <option value='WI'>Wisconsin</option>
+      //       <option value='WY'>Wyoming</option>
+      //     </select>
+      //   </label>
+      //   <button onClick={handleSubmit}>
+      //     <Link to='/birds'>Submit</Link>
+      //   </button>
+      // </form>
     )
 }
 
