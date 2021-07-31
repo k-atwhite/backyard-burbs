37e9824ea1d16fd1c2bdc2c620803e8f137317ac
Sydney Bear <sbear3915@gmail.com>
2021-07-31 13:45:52 -0600

    Add mock data to fixutres

---
+++ cypress/fixtures/nuthatchImageMockData.js
@@ -0,0 +1,18 @@
+
+{
+  "photos": {
+    "photo": [
+      {
+        "id": "24112610958",
+        "owner": "92361032@N05",
+        "secret": "a75bcf9952",
+        "server": "4502",
+        "farm": 5,
+        "title": "*** White-breasted Nuthatch / Sittelle à poitrine blanche",
+        "ispublic": 1,
+        "isfriend": 0,
+        "isfamily": 0
+      }
+    ]
+  }
+}
