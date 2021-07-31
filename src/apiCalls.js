8c775583dfdcc4e608048272d9b67227ca7edf21
Sydney Bear <sbear3915@gmail.com>
2021-07-31 11:24:18 -0600

    Update loading message so it does not show during error

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,6 +9,6 @@ export const getBirds = (currentState) => {
 export const getImages = (birdName, num) => {
   const formattedName = birdName.replace(' ', '%20');
 
-  return fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=${num}&format=json&nojsoncallback=1&sort=relevance&text=${formattedName}%20bird`)
+  return fetch(`htps://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=${num}&format=json&nojsoncallback=1&sort=relevance&text=${formattedName}%20bird`)
     .then(response => response.json())
 }
