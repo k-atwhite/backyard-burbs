11ed0fea17bb177b1adf550bd47a483c117f610c
Sydney Bear <sbear3915@gmail.com>
2021-08-01 16:19:10 -0600

    Test that nav has list of birds when state is clicked

--- cypress/integration/navBar_spec.js
+++ cypress/integration/navBar_spec.js
@@ -1,6 +1,18 @@
 describe("Navigation Bar", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    cy.fixture('birdListMockData').then((testMyBirds) => {
+      cy.intercept('https://api.ebird.org/v2/data/obs/US-CO/recent', testMyBirds)
+     })
+     cy.fixture("nuthatchImageMockData").then((nuthatch) => {
+       cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Sitta%20carolinensis%20bird", nuthatch);
+     });
+     cy.fixture("spoonbillImageMockData").then((spoonbill) => {
+       cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Platalea%20ajaja%20bird", spoonbill);
+     });
+     cy.fixture("treeSparrowImageMockData").then((sparrow) => {
+       cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Passer%20montanus%20bird", sparrow);
+     });
+     cy.visit("http://localhost:3000/");
   });
 
 
@@ -27,4 +39,10 @@ describe("Navigation Bar", () => {
       cy.get("header")
         .get(".burb-logo").should('exist').should('be.visible')
     })
-  });
+
+    it("Should show List of Birds nav link when state is chosen", () => {
+      cy.get("path[class='CO state']").click();
+      cy.get("nav").contains("List of Birds")
+    })
+});
+
