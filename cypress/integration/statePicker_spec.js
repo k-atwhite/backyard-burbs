a5a98b4b96035fd08fc2e77642f032b473461a02
Lauren Kessell <lkessell1@gmail.com>
2021-07-31 20:12:04 -0400

    Add tests for map visibility, active nav link

--- cypress/integration/statePicker_spec.js
+++ cypress/integration/statePicker_spec.js
@@ -3,26 +3,17 @@ beforeEach(() => {
 });
 
 describe("State Picker", () => {
-  it("Should visit the home page", () => {
-    cy.contains("Backyard Burbs");
+  it("A map of the US should be visible on the home page", () => {
+    cy.get(".us-state-map").should("be.visible");
   });
 
-  it("Should show a drop down form", () => {
-    cy.get(".form").should("be.visible");
+  it("The Choose State nav link should be active", () => {
+    cy.contains(".nav-link", "Choose State").should("have.class", "selected-link");
   });
 
-  it("Should show all 50 states", () => {
-    // Change to options are length of 50 - something like that
-    cy.get("option[value=AK]").get("option[value=WY]");
-  });
-
-  it("Should have a submit button", () => {
-    cy.get("button").contains("Submit");
-  });
-
-  it("Should direct user to list of all birds after clicking submit", () => {
-    cy.get("button").click();
-    // I THINK I NEED TO CHOOSE A STATE
-    // AND THEN MAKE SURE THE URL MATCHES
-  });
+  // it("Should direct user to list of all birds after clicking submit", () => {
+  //   cy.get("button").click();
+  //   // I THINK I NEED TO CHOOSE A STATE
+  //   // AND THEN MAKE SURE THE URL MATCHES
+  // });
 });
