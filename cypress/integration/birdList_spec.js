fca78eb3d6e522e07b2967f3945ea2f79a832364
Lauren Kessell <lkessell1@gmail.com>
2021-08-01 11:26:55 -0400

    Add test for card image and title

--- cypress/integration/birdList_spec.js
+++ cypress/integration/birdList_spec.js
@@ -20,20 +20,16 @@ beforeEach(() => {
 });
 
 describe("Regional Bird List", () => {
-  it("Should display list of birds in chosen state", () => {
-    cy.get("form")
-      .get("option[value=AK]")
-      .get("button")
-      .click()
-      .get("bird-card");
-    // SUGGESTIONS FOR ADDITIONAL TESTS? NOT SURE HOW MANY, NO SURE WHICH CARDS WILL SHOW UP...
+  it("Bird cards should show an image and the bird's common name", () => {
+    cy.get(".bird-card").should("have.css", "background-image")
+    cy.get(".bird-card").contains("Roseate Spoonbill")
   });
 
-  it("Should contain cards with the common name and latin name", () => {
-    // add in dummy data! Sydney will show us fixtures!
-    cy.get("bird-card").contains("sci-name");
-    get("bird-card").contains("common-name");
-  });
-
-  it("Each card should clickable to then display that birds details", () => {});
+  // it("Should contain cards with the common name and latin name", () => {
+  //   // add in dummy data! Sydney will show us fixtures!
+  //   cy.get("bird-card").contains("sci-name");
+  //   get("bird-card").contains("common-name");
+  // });
+  //
+  // it("Each card should clickable to then display that birds details", () => {});
 });
