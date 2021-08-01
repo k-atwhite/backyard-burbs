4575a908227a6516ffa285a58704b62955a9f3d2
Sydney Bear <sbear3915@gmail.com>
2021-08-01 16:03:51 -0600

    Add correct beforeeach for mybird test

--- cypress/integration/myBirds_spec.js
+++ cypress/integration/myBirds_spec.js
@@ -1,16 +1,28 @@
 describe("my-birds", () => {
   beforeEach(() => {
-     cy.fixture('birdListMockData.js').then((testMyBirds) => {
-       cy.intercept('https://api.ebird.org/v2/data/obs/US-FL/recent', testMyBirds)
-       cy.visit("http://localhost:3000/");
-       cy.get(".FL state").click();
-       cy.get("#whbnut").click()
+     cy.fixture('birdListMockData').then((testMyBirds) => {
+       cy.intercept('https://api.ebird.org/v2/data/obs/US-CO/recent', testMyBirds)
       })
       cy.fixture("nuthatchImageMockData").then((nuthatch) => {
         cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Sitta%20carolinensis%20bird", nuthatch);
-      });    
+      });
+      cy.fixture("spoonbillImageMockData").then((spoonbill) => {
+        cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Platalea%20ajaja%20bird", spoonbill);
+      });
+      cy.fixture("treeSparrowImageMockData").then((sparrow) => {
+        cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Passer%20montanus%20bird", sparrow);
+      });
+      cy.visit("http://localhost:3000/");
+      cy.get("path[class='CO state']").click();
+      cy.get("#whbnut").click();
+      cy.get(".i-see-it").click();  
     });
       it("Should contain a bird card when I see it button is clicked", () => {
-        cy.get('.bird-card')
+        cy.get('#whbnut')
       });
+      it("Should have an image, and a common name", () => {
+        cy.get('#whbnut')
+          .get('.card-text').contains('White-breasted Nuthatch')
+          .get()
+      })
 })
