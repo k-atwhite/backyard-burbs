e339e49b22fb57872e677e592847b5329f18116d
Lauren Kessell <lkessell1@gmail.com>
2021-08-01 11:08:02 -0400

    Update beforeEach

--- cypress/integration/birdList_spec.js
+++ cypress/integration/birdList_spec.js
@@ -1,10 +1,22 @@
 beforeEach(() => {
-  cy.visit("http://localhost:3000/").intercept(
-    "GET",
-    "https://api.ebird.org/v2/data/obs/US-AK/recent`"
-  );
-  // INTERCEPT NEEDS TO GO HERE - it's just listening for the request
-  // Pop in three birds from alaska
+  cy.fixture("birdListMockData").then((listData) => {
+    cy.intercept("https://api.ebird.org/v2/data/obs/US-CO/recent", listData);
+  });
+
+  cy.fixture("nuthatchImageMockData").then((nuthatch) => {
+    cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Sitta%20carolinensis%20bird", nuthatch);
+  });
+
+  cy.fixture("spoonbillImageMockData").then((spoonbill) => {
+    cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Platalea%20ajaja%20bird", spoonbill);
+  });
+
+  cy.fixture("treeSparrowImageMockData").then((sparrow) => {
+    cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Passer%20montanus%20bird", sparrow);
+  });
+
+  cy.visit("http://localhost:3000/");
+  cy.get("path[class='CO state']").click();
 });
 
 describe("Regional Bird List", () => {
