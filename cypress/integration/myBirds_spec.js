132da3c7bd5c04c75c5d8c6a63570d82f8e81f44
Sydney Bear <sbear3915@gmail.com>
2021-08-01 14:14:27 -0600

    Add testing for mybird

--- cypress/integration/myBirds_spec.js
+++ cypress/integration/myBirds_spec.js
@@ -1,30 +1,16 @@
-beforeEach(() => {
-  
-  cy.visit("http://localhost:3000/");
-});
-
-describe("Navigation Bar", () => {
-  it("Should contain link to State Picker", () => {
-    cy.get("nav")
-      .contains("Choose State")
-      .click()
-      .url("http://localhost:3000/");
-  });
-
-  it("Should contain link to list My Birds", () => {
-    cy.get("nav")
-      .contains("My Birds")
-      .click()
-      .url("http://localhost:3000/myBirds");
-  });
-
-  it("Should have a title", () => {
-    cy.get("header")
-      .contains("Backyard Burbs")
-  });
-  
-  it("Should have a bird image", () => {
-    cy.get("header")
-      .get(".burb-logo").should('exist').should('be.visible')
-  })
-});
+describe("my-birds", () => {
+  beforeEach(() => {
+     cy.fixture('birdListMockData.js').then((testMyBirds) => {
+       cy.intercept('https://api.ebird.org/v2/data/obs/US-FL/recent', testMyBirds)
+       cy.visit("http://localhost:3000/");
+       cy.get(".FL state").click();
+       cy.get("#whbnut").click()
+      })
+      cy.fixture("nuthatchImageMockData").then((nuthatch) => {
+        cy.intercept("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=50906f9c24e50a02f0d6034a3c6df6d7&per_page=1&format=json&nojsoncallback=1&sort=relevance&text=Sitta%20carolinensis%20bird", nuthatch);
+      });    
+    });
+      it("Should contain a bird card when I see it button is clicked", () => {
+        cy.get('.bird-card')
+      });
+})
